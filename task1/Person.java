f8167c7a295d5879803a442404cabf129d0484c5
Ningrui Zhang <ningrui1227@gmail.com>
2025-03-14 13:50:25 +0000

    add age field

--- task1/Person.java
+++ task1/Person.java
@@ -2,9 +2,11 @@ package task1;
 
 public class Person {
     private String name;
+    private int age;
 
     public Person(String name, int age) {
         this.name = name;
+        this.age = age;
     }
 
     public String getName() {
