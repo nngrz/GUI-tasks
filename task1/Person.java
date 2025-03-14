aa54c1566118ea4d26fb25e81e491e31bb49924c
Ningrui Zhang <ningrui1227@gmail.com>
2025-03-14 12:47:54 +0000

    remove not used field in Person.java

--- task1/Person.java
+++ task1/Person.java
@@ -2,11 +2,9 @@ package task1;
 
 public class Person {
     private String name;
-    private int age;
 
     public Person(String name, int age) {
         this.name = name;
-        this.age = age;
     }
 
     public String getName() {
