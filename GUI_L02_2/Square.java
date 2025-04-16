9210b408804d0bb0776e04b3f24dfc68fd7d5ebd
Ningrui Zhang <ningrui1227@gmail.com>
2025-04-16 11:32:08 +0100

    remove not used field in Square.java

--- GUI_L02_2/Square.java
+++ GUI_L02_2/Square.java
@@ -1,10 +1,8 @@
 package GUI_L02_2;
 
 class Square extends Rectangle {
-    private double side;
 
     public Square(double side, String color) {
         super(side, side, color); // Call the Rectangle constructor with equal length and width 
-        this.side = side;
     }
 }
