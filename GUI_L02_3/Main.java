ddb766bd22f048a8489235d6a9a62fb536656280
Ningrui Zhang <ningrui1227@gmail.com>
2025-04-19 23:01:30 +0100

    modify Main.java and Square.java

--- GUI_L02_3/Main.java
+++ GUI_L02_3/Main.java
@@ -3,24 +3,28 @@ import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
-        Square[] squares = {
-            new Square(8),
-            new Square(3),
-            new Square(4),
-            new Square(2),
-            new Square(6),
-        };
-        
-        System.out.println("Before sorting: ");
-        for (Square s : squares) {
-            System.out.println(s);
-        }
-
-        Arrays.sort(squares);
-
-        System.out.println("After sorting: ");
-        for (Square s : squares) {
-            System.out.println(s);
+        try {
+            Square[] squares = {
+                new Square(5),
+                new Square(3),
+                new Square(4),
+                new Square(2),
+                new Square(6),
+            };
+            
+            System.out.println("Before sorting: ");
+            for (Square s : squares) {
+                System.out.println(s);
+            }
+    
+            Arrays.sort(squares);
+    
+            System.out.println("After sorting: ");
+            for (Square s : squares) {
+                System.out.println(s);
+            }
+        } catch (TooBigSquareException e) {
+            System.err.println("Error: " + e.getMessage());
         }
     }
 }
--- GUI_L02_3/Square.java
+++ GUI_L02_3/Square.java
@@ -1,20 +1,29 @@
 package GUI_L02_3;
 
-public class Square implements Comparable<Square> {
+public class Square implements Comparable<Square>, Figure {
     private int length;
     private int number;
 
-    static int counter = 0;
+    public static int counter = 0;
 
-    public Square(int length) {
+    public Square(int length) throws TooBigSquareException {
+        if (length > max) {
+            throw new TooBigSquareException("Maximum length is " + max);
+        }
         this.length = length;
         this.number = ++counter;
     }
 
+    @Override
     public int getArea() {
         return length * length;
     }
 
+    @Override 
+    public int getPerimeter() {
+        return 4 * length;
+    }
+    
     @Override
     public String toString() {
         return "(" + number + "):" + getArea();
