b7b46a75c5805c67e0d52f0a92141dba3b9463ff
Ningrui Zhang <ningrui1227@gmail.com>
2025-05-22 10:53:15 +0800

    add GUI_L08_3 package

---
+++ GUI_L08_3/Main.java
@@ -0,0 +1,9 @@
+package GUI_L08_3;
+
+import javax.swing.SwingUtilities;
+
+public class Main {
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(() -> new CalculatorWindow());
+    }
+}
