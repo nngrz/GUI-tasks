07498d8b9e02fce5eb1f0d7e0e3a905c9777dc53
Ningrui Zhang <ningrui1227@gmail.com>
2025-06-12 19:41:10 +0100

    add exception handling

--- GUI_L05/Factorial.java
+++ GUI_L05/Factorial.java
@@ -3,6 +3,10 @@ package GUI_L05;
 public class Factorial {
 
     public static int factorial(int n) {
+        if (n < 1) {
+            throw new IllegalArgumentException("Factorial is undefined for negative numbers");
+        }
+
         if (n > 1) {
             return n * factorial(n - 1);
         }
