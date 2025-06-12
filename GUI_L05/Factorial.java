9ec078dd171d2c716e56c207804c44e92bd0764a
Ningrui Zhang <ningrui1227@gmail.com>
2025-06-12 19:37:19 +0100

    add Factorial.java

---
+++ GUI_L05/Factorial.java
@@ -0,0 +1,16 @@
+package GUI_L05;
+
+public class Factorial {
+
+    public static int factorial(int n) {
+        if (n > 1) {
+            return n * factorial(n - 1);
+        }
+        return 1;
+    }
+
+    public static void main(String[] args) {
+        int number = 5;
+        System.out.println(factorial(number));
+    }
+}
