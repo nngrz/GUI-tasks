f75cd70c3ba4b6456788375fd652c43b33b383a8
Ningrui Zhang <ningrui1227@gmail.com>
2025-06-28 15:08:33 +0100

    add package SingerManager

---
+++ exam/SingerManager/Main.java
@@ -0,0 +1,31 @@
+package exam.SingerManager;
+
+public class Main {
+    public static void main(String[] args){
+        Singer s1 = new Singer("Eminem"){
+            @Override
+            public String sing() {
+                return "You own it, you better never let it go";
+            }
+        };
+
+        Singer s2 = new Singer("Eagles"){
+            @Override
+            public String sing() {
+                return "Hotel California";
+            }
+        };
+
+        Singer s3 = new Singer("Simon & Garfunkel"){
+            @Override
+            public String sing() {
+                return "Hello darkness , my old friend . I've come to talk with you again";
+            }
+        };
+
+        Singer sp [] = {s1,s2,s3};
+        for (Singer s : sp)
+        System.out.println(s);
+        System.out.println("\n"+Singer.loudest(sp));
+    }
+}
