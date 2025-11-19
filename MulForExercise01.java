21574844c627cc427600f3a84ff79340d4c00ed8
Ningrui Zhang <ningrui1227@gmail.com>
2025-11-19 18:39:27 +0000

    add new function: count passed student number

--- MulForExercise01.java
+++ MulForExercise01.java
@@ -5,10 +5,13 @@ public class MulForExercise01 {
     public static void main(String[] args) {
         Scanner myObj = new Scanner(System.in);
 
-        int classNumber = 2;
+        int classNumber = 3;
         int studentNumberEachClass = 5;
 
         int sumAllClasses = 0;
+
+        int scorePass = 60;
+        int countPass = 0;
         // Loop through classes
         for (int i = 1; i <= classNumber; i++) {
             int sumEachClass = 0; // Reset sum of each class to 0
@@ -17,8 +20,12 @@ public class MulForExercise01 {
             for (int j = 1; j <= studentNumberEachClass; j++) {
                 System.out.println("Enter score for student " + j);
 
-                int scoreEachClass = myObj.nextInt();
-                sumEachClass += scoreEachClass;
+                int scoreEachStudent = myObj.nextInt();
+
+                if (scoreEachStudent >= scorePass) {
+                    countPass++;
+                }
+                sumEachClass += scoreEachStudent;
             }
 
             int avgEachClass = sumEachClass / studentNumberEachClass;
@@ -29,6 +36,7 @@ public class MulForExercise01 {
 
         int avgAllClasses = sumAllClasses / (classNumber*studentNumberEachClass);
         System.out.println("Average score for all class is " + avgAllClasses);
+        System.out.println("There are " + countPass + " students passed");
 
         myObj.close();
     }
